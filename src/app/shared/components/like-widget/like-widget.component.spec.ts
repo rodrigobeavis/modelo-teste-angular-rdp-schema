60bc4da910a579853f5231f650da45701f0ceed5
Rodrigo Pedroza Barcelos <rodrigobeavis@yahoo.com.br>
2022-06-21 22:21:11 -0300

    refatorar e testar componente LikeWidget

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,25 +1,82 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LikeWidgetComponent } from './like-widget.component';
+import { UniqueIdService } from './../../services/unique-id/unique-id.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-describe('LikeWidgetComponent', () => {
+
+describe(LikeWidgetComponent.name, () => {
   let component: LikeWidgetComponent;
-  let fixture: ComponentFixture<LikeWidgetComponent>;
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ]
+      imports: [
+        FontAwesomeModule
+      ],
+      declarations: [LikeWidgetComponent],
+      providers: [
+        UniqueIdService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LikeWidgetComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.detectChanges(); // dectetar as alterações do component
   });
 
-  it('should create', () => {
+  it('should create componente', () => {
+    component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
+
+  it(`Should NOT auto-generate ID during ngOnInit when (@input id
+      ) property is assinged`, () => {
+
+    component = fixture.componentInstance;
+    expect(component.id).toBeTruthy();
+  });
+
+
+  it(`Should NOT auto-generate ID during ngOnInit when (@input id
+    ) property is assinged`, () => {
+    component = fixture.componentInstance;
+    component.id = 'someId';
+    expect(component.id).toBe('someId');
+  });
+
+  // it(`#${LikeWidgetComponent.prototype.like.name}
+  //   Should trigger when called`, () => {
+  //   fixture.detectChanges();
+  //   component = fixture.componentInstance;
+  //   component.liked.subscribe(() => {
+  //     expect(true).toBeTrue();
+  //   });
+  //   component.like();
+  // });
+
+  //   it(`#${LikeWidgetComponent.prototype.like.name}
+  //   Should trigger when called`, done => {
+  //   fixture.detectChanges();
+  //   component = fixture.componentInstance;
+  //   component.liked.subscribe(() => {
+  //     expect(true).toBeTrue();
+  //     done();
+  //   });
+  //   component.like();
+  // });
+
+
+  it(`#${LikeWidgetComponent.prototype.like.name}
+  Should trigger (@Output liked) when called`, () => {
+    spyOn(component.liked, 'emit');
+    fixture.detectChanges();
+    component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
+  });
+
+
+
 });
